2ffc8b19ce4dfdaa2b8375fe246d371667f1a12e
Joecey <jsaloma@my.hpu.edu>
2020-11-17 21:03:50 -1000

    Feat: Create constants to centralize configuration
    Create Constants.java file to centralize configuration of application
    for easy quick modification to setup and redirect output as necessary.

---
+++ src/Constants.java
@@ -0,0 +1,29 @@
+//Operating Systems Distributed System package
+package osdistributedsystem;
+
+public interface Constants {
+    //Number of clients
+    public static final int NUMBER_OF_CLIENTS = 5;
+
+    public static final int PORT = 8020;
+    
+    public static final int READ_VALUE = 80;
+    public static final int WRITE_VALUE = 20;
+    
+    //Log File Names
+    public static final String FILE_LOG_GLOBAL = "global.log";
+    public static final String FILE_LOG_LOCAL = "local.log";
+    public static final String FILE_TARGET = "target.txt";
+    public static final String FILE_LOG_BACKGROUND = "background.log";
+
+    //Directories
+    public static final String DIRECTORY_LOG = "data/";
+    public static final String DIRECTORY_CONTROLLER = Constants.DIRECTORY_LOG + "controller/";
+    public static final String[] DIRECTORY_PC = new String[] {
+        Constants.DIRECTORY_LOG + "PC1/",
+        Constants.DIRECTORY_LOG + "PC2/",
+        Constants.DIRECTORY_LOG + "PC3/",
+        Constants.DIRECTORY_LOG + "PC4/",
+        Constants.DIRECTORY_LOG + "PC5/"
+    };  
+}
