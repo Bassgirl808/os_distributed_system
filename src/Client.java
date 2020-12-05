14d064003b2f230323ca127eebdd117d0b9a9c15
Joecey <jsaloma@my.hpu.edu>
2020-12-04 20:25:29 -1000

    Feat: Set up operator and instructor in client
    Receive Vectorclock from server. Create operator thread to handle
    receiving messages from server. Create instructor thread to handle
    sending messages to server for reading and writing.

--- src/Client.java
+++ src/Client.java
@@ -2,6 +2,7 @@ package osdistributedsystem;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.lang.ClassNotFoundException;
 
 import java.net.Socket;
 
@@ -11,9 +12,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import java.util.concurrent.Semaphore;
-
-import osdistributedsystem.FileLogger;
-import osdistributedsystem.VectorClock;
 
 import java.util.Vector;
 
@@ -47,14 +45,36 @@ public class Client implements Runnable {
     public void run() {
         FileLogger.writeClient(this.id, "[INFO]:[Client#run]::Starting Client");
         try {
-            FileLogger.writeClient(this.id, "[INFO]:[Client#Client]::Setting up socket");
+            FileLogger.writeClient(this.id, "[INFO]:[Client#run]::Setting up socket");
             this.socket = new Socket(Constants.IPV4, Constants.PORT);
-            FileLogger.writeClient(this.id, "[INFO]:[Client#Client]::Socket has been set up");
+            FileLogger.writeClient(this.id, "[INFO]:[Client#run]::Socket has been set up");
 
-            FileLogger.writeClient(this.id, "[INFO]:[Client#Client]::Connecting streams");
+            FileLogger.writeClient(this.id, "[INFO]:[Client#run]::Connecting streams");
             ObjectInputStream input = new ObjectInputStream(this.socket.getInputStream());
             ObjectOutputStream output = new ObjectOutputStream(this.socket.getOutputStream());
-            FileLogger.writeClient(this.id, "[INFO]:[Client#Client]::Streams connected");
+            FileLogger.writeClient(this.id, "[INFO]:[Client#run]::Streams connected");
+
+            FileLogger.writeClient(this.id, "[INFO]:[Client#run]::Receive Vectorclock");
+            this.clock = (VectorClock)input.readObject();
+            FileLogger.writeClient(this.id, "[INFO]:[Client#run]::VectorClock received");
+
+            FileLogger.writeClient(this.id, "[INFO]:[Client#run]::Creating operator to respond to serverthread communications");
+            Operator operator = new Operator(this.clock, this.socket, input, output, null);
+            Thread operatorThread = new Thread(operator, Constants.THREAD_NAME_OPERATOR);
+            ShutdownHandler.Operators.add(operatorThread);
+            FileLogger.writeClient(this.id, "[INFO]:[Client#run]::Operator created");
+            
+            FileLogger.writeClient(this.id, "[INFO]:[Client#run]::Creating instructor to send instructions to serverthreads for requesting reads and writes");
+            Instructor instructor = new Instructor(this.clock, this.socket, input, output, null);
+            Thread instructorThread = new Thread(instructor, Constants.THREAD_NAME_INSTRUCTOR);
+            ShutdownHandler.Instructors.add(instructorThread);
+            FileLogger.writeClient(this.id, "[INFO]:[Client#run]::Instructor created");
+
+            FileLogger.writeClient(this.id, "[INFO]:[Client#run]::Starting threads");
+            operatorThread.start();
+            instructorThread.start();
+            FileLogger.writeClient(this.id, "[INFO]:[Client#run]::Started threads");
+
 
             while (true) {
                 /*
@@ -74,10 +94,10 @@ public class Client implements Runnable {
                 * process whole queue
                 */
             }
-        } /*catch (ClassNotFoundException cnfex) {
+        } catch (ClassNotFoundException cnfex) {
             FileLogger.writeClient(this.id, "[ERROR]:[Client#run]::Class Not Found: " + cnfex.getMessage());
             System.err.println(cnfex.getMessage());
-        }*/ catch (UnknownHostException uhex) {
+        } catch (UnknownHostException uhex) {
             FileLogger.writeClient(this.id, "[ERROR]:[Client#run]::Unknown Host: " + uhex.getMessage());
             System.err.println(uhex.getMessage());
         } catch (IOException ioex) {
