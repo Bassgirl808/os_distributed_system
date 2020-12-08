375b8f26462bda138a200979557be56a5bd02ff5
Joecey <jsaloma@my.hpu.edu>
2020-12-08 02:00:47 -1000

    Fix: Ensure proper output streams are referenced
    Use this. identifier to ensure proper stream references.

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -45,12 +45,12 @@ public class ServerThread implements Runnable {
         while (this.serverThreads.size() != Constants.NUMBER_OF_CLIENTS);
         try {
             FileLogger.writeServerThread(this.getId(), "[INFO]:[ServerThread#run]::Sending clock to client");
-            output.writeObject(this.clock);
+            this.output.writeObject(this.clock);
             FileLogger.writeServerThread(this.getId(), "[INFO]:[ServerThread#run]::Sent clock: " + this.getClock() + " ID: " + this.getId());
             //if (this.clock.getId() != this.getId()) throw new Exception("Invalid clock, id mismatch");
             
-            Command command;
-            ObjectOutputStream output;
+            Command command = null;
+            ObjectOutputStream output = null;
 
             while ((command = Command.values()[input.readInt()]) != null && !Thread.currentThread().interrupted()) {
                 this.clock = (VectorClock)input.readObject();
@@ -58,33 +58,37 @@ public class ServerThread implements Runnable {
                     case REQUEST_READ:
                         output = this.serverThreads.get(1).getObjectOutputStream();
                         output.writeInt(Command.REQUEST_READ.ordinal());
-                        output.writeObject(this.clock);
+                        output.writeObject(this.getClock());
                         output.flush();
+                        output.reset();
                         break;
                     case REPLY_READ:
                         output = this.serverThreads.get(input.readInt()).getObjectOutputStream();
                         output.writeInt(Command.REPLY_READ.ordinal());
-                        output.writeObject(this.clock);
+                        output.writeObject(this.getClock());
                         output.flush();
+                        output.reset();
                         break;
                     case REQUEST_WRITE:
                         output = this.serverThreads.get(this.getId()).getObjectOutputStream();
                         output.writeInt(Command.REQUEST_WRITE.ordinal());
                         output.writeObject(this.getClock());
                         output.flush();
+                        output.reset();
                         break;
                     case REPLY_WRITE:
                         output = this.serverThreads.get(input.readInt()).getObjectOutputStream();
                         output.writeInt(Command.REPLY_WRITE.ordinal());
                         output.writeObject(this.getClock());
                         output.flush();
+                        output.reset();
                         break;
                 }
                 output = null;
             }
             this.socket.close();
             this.input.close();
-            this.output.close();
+            output.close();
         } catch (ClassNotFoundException cnfex) {
             FileLogger.writeServerThread(this.getId(), "[ERROR]:[ServerThread#run]::Class Not Found: " + cnfex.getMessage());
             System.err.println(cnfex.getMessage());
@@ -109,6 +113,6 @@ public class ServerThread implements Runnable {
     }
     
     public ObjectOutputStream getObjectOutputStream() {
-        return this.output;
+        return output;
     }
 }
