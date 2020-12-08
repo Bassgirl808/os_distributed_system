2a63d4f4c0b247b95912ea235b4281999a8853fc
Joecey <jsaloma@my.hpu.edu>
2020-12-08 00:20:05 -1000

    Feat: Transmit command enum and wait for join
    Transmit command enum through conversion to ordinal value over object
    streams as int. Refactor object streams to match application style. Hook
    simulation cycle into shutdown handler to complete cycle and check for
    join request

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -16,11 +16,10 @@ public class ServerThread implements Runnable {
     private HashMap<Integer, ServerThread> serverThreads;
     private Socket socket = null;
 
-    private ObjectInputStream objectInput = null;
-    private ObjectOutputStream objectOutput = null;
+    private ObjectInputStream input = null;
+    private ObjectOutputStream output = null;
 
     private VectorClock clock;
-    private Semaphore lock;
     
     public ServerThread(int id, HashMap<Integer, ServerThread> serverThreads, Socket socket) {
         FileLogger.writeServerThread(id, "[INFO]:[ServerThread#ServerThread]::Creating ServerThread");
@@ -28,8 +27,8 @@ public class ServerThread implements Runnable {
         this.socket = socket;
         FileLogger.writeServerThread(id, "[INFO]:[ServerThread#ServerThread]::Initialize object streams for " + id);
         try {
-            this.objectInput = new ObjectInputStream(this.socket.getInputStream());
-            this.objectOutput = new ObjectOutputStream(this.socket.getOutputStream());
+            this.input = new ObjectInputStream(this.socket.getInputStream());
+            this.output = new ObjectOutputStream(this.socket.getOutputStream());
         } catch (IOException ioex) {
             FileLogger.writeServerThread(id, "[ERROR]:[ServerThread#ServerThread]::ServerThread Failure: Could not initialize object streams: " + ioex.getMessage());
             System.err.println(ioex.getMessage());
@@ -37,7 +36,6 @@ public class ServerThread implements Runnable {
         }
         FileLogger.writeServerThread(id, "[INFO]:[ServerThread#ServerThread]::Initialized object streams for " + id);
         this.clock = new VectorClock(id);
-        this.lock = new Semaphore(1);
         FileLogger.writeServerThread(id, "[INFO]:[ServerThread#ServerThread]::Created ServerThread");
     }
     
@@ -47,37 +45,37 @@ public class ServerThread implements Runnable {
         while (this.serverThreads.size() != Constants.NUMBER_OF_CLIENTS);
         try {
             FileLogger.writeServerThread(this.getId(), "[INFO]:[ServerThread#run]::Sending clock to client");
-            objectOutput.writeObject(this.clock);
+            output.writeObject(this.clock);
             FileLogger.writeServerThread(this.getId(), "[INFO]:[ServerThread#run]::Sent clock: " + this.getClock() + " ID: " + this.getId());
             //if (this.clock.getId() != this.getId()) throw new Exception("Invalid clock, id mismatch");
             
             Command command;
             ObjectOutputStream output;
 
-            while ((command = (Command)objectInput.readObject()) != null) {
-                this.clock = (VectorClock)objectInput.readObject();
+            while ((command = Command.values()[input.readInt()]) != null && !Thread.currentThread().interrupted()) {
+                this.clock = (VectorClock)input.readObject();
                 switch (command) {
                     case REQUEST_READ:
                         output = this.serverThreads.get(1).getObjectOutputStream();
-                        output.writeObject(Command.REQUEST_READ);
+                        output.writeInt(Command.REQUEST_READ.ordinal());
                         output.writeObject(this.clock);
                         output.flush();
                         break;
                     case REPLY_READ:
-                        output = this.serverThreads.get(objectInput.readInt()).getObjectOutputStream();
-                        output.writeObject(Command.REPLY_READ);
+                        output = this.serverThreads.get(input.readInt()).getObjectOutputStream();
+                        output.writeInt(Command.REPLY_READ.ordinal());
                         output.writeObject(this.clock);
                         output.flush();
                         break;
                     case REQUEST_WRITE:
                         output = this.serverThreads.get(this.getId()).getObjectOutputStream();
-                        output.writeObject(Command.REQUEST_WRITE);
+                        output.writeInt(Command.REQUEST_WRITE.ordinal());
                         output.writeObject(this.getClock());
                         output.flush();
                         break;
                     case REPLY_WRITE:
-                        output = this.serverThreads.get(objectInput.readInt()).getObjectOutputStream();
-                        output.writeObject(Command.REPLY_WRITE);
+                        output = this.serverThreads.get(input.readInt()).getObjectOutputStream();
+                        output.writeInt(Command.REPLY_WRITE.ordinal());
                         output.writeObject(this.getClock());
                         output.flush();
                         break;
@@ -85,8 +83,8 @@ public class ServerThread implements Runnable {
                 output = null;
             }
             this.socket.close();
-            this.objectInput.close();
-            this.objectOutput.close();
+            this.input.close();
+            this.output.close();
         } catch (ClassNotFoundException cnfex) {
             FileLogger.writeServerThread(this.getId(), "[ERROR]:[ServerThread#run]::Class Not Found: " + cnfex.getMessage());
             System.err.println(cnfex.getMessage());
@@ -107,10 +105,10 @@ public class ServerThread implements Runnable {
     }
 
     public ObjectInputStream getObjectInputStream() {
-        return this.objectInput;
+        return this.input;
     }
     
     public ObjectOutputStream getObjectOutputStream() {
-        return this.objectOutput;
+        return this.output;
     }
 }
